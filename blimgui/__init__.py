9917ebc5ecb406b58ae4f75f1fe49c427c3bdb56
Justin Sostmann <justin.sostmann@googlemail.com>
2025-01-08 21:00:48 +0100

    Change draw loop

--- blimgui/__init__.py
+++ blimgui/__init__.py
@@ -1,7 +1,10 @@
 import site
 from collections.abc import Callable
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, cast
+from typing import Any
+
+from mods_base.hook import Type, hook
+from unrealsdk import unreal
 
 site.addsitedir(str(Path(__file__).parent.absolute() / "dist"))
 
@@ -10,17 +13,10 @@ site.addsitedir(str(Path(__file__).parent.absolute() / "dist"))
 # py -V:3.13-32 -m pip install -v . -t "./dist"
 # monkey patched pyglet.gl.win32.py:32 to never use WGL_ARB_pixel_format
 
-from imgui_bundle import imgui
+from imgui_bundle import hello_imgui, imgui, immapp
+from imgui_bundle import icons_fontawesome_4 as icons
 from mods_base import Library, build_mod, options
 from mods_base.keybinds import keybind
-from unrealsdk import logging, unreal
-from unrealsdk.hooks import Type, add_hook, remove_hook
-
-if TYPE_CHECKING:
-    import pyglet  # type: ignore
-    from imgui_bundle.python_backends import pyglet_backend
-    from pyglet import gl  # type: ignore
-
 
 __version__: str
 __version_info__: tuple[int, ...]
@@ -32,63 +28,48 @@ IMPL = None
 size_x = options.HiddenOption("size_x", 1280)
 size_y = options.HiddenOption("size_y", 720)
 
+ALL_THEMES = [
+    hello_imgui.ImGuiTheme_.darcula_darker,
+    hello_imgui.ImGuiTheme_.darcula,
+    hello_imgui.ImGuiTheme_.imgui_colors_classic,
+    hello_imgui.ImGuiTheme_.imgui_colors_dark,
+    hello_imgui.ImGuiTheme_.imgui_colors_light,
+    hello_imgui.ImGuiTheme_.material_flat,
+    hello_imgui.ImGuiTheme_.photoshop_style,
+    hello_imgui.ImGuiTheme_.gray_variations,
+    hello_imgui.ImGuiTheme_.gray_variations_darker,
+    hello_imgui.ImGuiTheme_.microsoft_style,
+    hello_imgui.ImGuiTheme_.cherry,
+    hello_imgui.ImGuiTheme_.light_rounded,
+    hello_imgui.ImGuiTheme_.so_dark_accent_blue,
+    hello_imgui.ImGuiTheme_.so_dark_accent_yellow,
+    hello_imgui.ImGuiTheme_.so_dark_accent_red,
+    hello_imgui.ImGuiTheme_.black_is_black,
+    hello_imgui.ImGuiTheme_.white_is_white,
+]
 
-def create_window(
-    caption: str,
-    width: int | options.HiddenOption = size_x,
-    height: int | options.HiddenOption = size_y,
-    resizable: bool = True,
-) -> None:
+ALL_THEMES_NAMES = [theme.name for theme in ALL_THEMES]
+
+
+def style_ui(_option: options.SpinnerOption | None = None, val: str = "") -> None:
     """
-    Create a new window with the given parameters. If a window already exists, rename only.
+    Apply the selected theme to the UI.
 
-    :param caption: The caption of the window
-    :param width: The width of the window
-    :param height: The height of the window
-    :param resizable: If True, window is resizable, else cannot be resized.
     :return: None
     """
-    global WINDOW, IMPL  # noqa: PLW0603
-    if not WINDOW and not IMPL:
-        WINDOW = pyglet.window.Window(
-            width=width if isinstance(width, int) else width.value,
-            height=height if isinstance(height, int) else height.value,
-            resizable=resizable,
-            caption=caption,
-            config=pyglet.gl.Config(),
-        )
-        gl.glClearColor(0.1, 0.1, 0.1, 1)
-        imgui.create_context()
-        IMPL = pyglet_backend.create_renderer(WINDOW)
-
-        def on_resize(w: int, h: int) -> None:
-            size_x.value = w
-            size_y.value = h
-
-        cast(pyglet.window.BaseWindow, WINDOW).push_handlers(on_resize)
-
-    elif WINDOW:
-        WINDOW.set_caption(caption=caption)
+    if not WINDOW:
+        return
+    theme = ALL_THEMES[ALL_THEMES_NAMES.index(val or imgui_theme.value)]
+    hello_imgui.apply_theme(theme)
 
 
-def close_window() -> bool:
-    """
-    Close the current window if any is open.
-
-    :return: Bool, True if window successfully closed, else False
-    """
-    global WINDOW, IMPL, ACTIVE_CALLBACK  # noqa: PLW0603
-    try:
-        cast(pyglet_backend.PygletRenderer, IMPL).shutdown()
-        cast(pyglet.window.BaseWindow, WINDOW).close()
-
-        WINDOW = None
-        IMPL = None
-        ACTIVE_CALLBACK = update
-        return True
-    except Exception as e:  # noqa: BLE001
-        logging.error(f"Error closing window: {e}")
-        return False
+imgui_theme = options.SpinnerOption(
+    "Theme",
+    ALL_THEMES_NAMES[0],
+    ALL_THEMES_NAMES,
+    wrap_enabled=True,
+    on_change=style_ui,
+)
 
 
 def update() -> None:
@@ -101,32 +82,46 @@ def update() -> None:
         imgui.end_main_menu_bar()
 
     imgui.begin("Hello World", True)
-    imgui.text("This is a text!")
+    imgui.text("This is a text!" + icons.ICON_FA_HEART)
     imgui.text_colored((0.2, 1.0, 0.0, 1.0), "Colored Text, wow!")
 
     imgui.end()
 
 
-def draw() -> None:
-    """
-    Calls the currently active callback function to update the rendered content in the new window.
-
-    :return: None
-    """
-    if not WINDOW or not IMPL:
-        return
-    cast(pyglet_backend.PygletRenderer, IMPL).process_inputs()
-    imgui.new_frame()
-    ACTIVE_CALLBACK()
-    if WINDOW:
-        WINDOW.clear()
-        imgui.render()
-        cast(pyglet_backend.PygletRenderer, IMPL).render(imgui.get_draw_data())
-    else:
-        imgui.end_frame()
-
-
 ACTIVE_CALLBACK = update
+
+
+def create_window(
+    caption: str,
+    width: int | options.HiddenOption = size_x,
+    height: int | options.HiddenOption = size_y,
+) -> None:
+    global should_close  # noqa: PLW0603
+    should_close = False
+    _x = width if isinstance(width, int) else width.value
+    _y = height if isinstance(height, int) else height.value
+    immapp.manual_render.setup_from_runner_params(
+        runner_params=immapp.RunnerParams(
+            fps_idling=hello_imgui.FpsIdling(fps_idle=0.0, enable_idling=False),
+            callbacks=hello_imgui.RunnerCallbacks(
+                show_gui=ACTIVE_CALLBACK,
+            ),
+            app_window_params=hello_imgui.AppWindowParams(
+                window_title=caption,
+                window_geometry=hello_imgui.WindowGeometry(size=(_x, _y)),
+            ),
+        ),
+        add_ons_params=None,
+    )
+    style_ui()
+
+
+should_close = False
+
+
+def close_window() -> None:
+    global should_close  # noqa: PLW0603
+    should_close = True
 
 
 def set_draw_callback(callback: DRAW_FUN) -> None:
@@ -140,41 +135,23 @@ def set_draw_callback(callback: DRAW_FUN) -> None:
     ACTIVE_CALLBACK = callback
 
 
+@hook("WillowGame.WillowGameViewportClient:Tick", Type.POST_UNCONDITIONAL, immediately_enable=True)
 def _on_tick(
     _obj: unreal.UObject,
     _args: unreal.WrappedStruct,
     _ret: Any,
     _func: unreal.BoundFunction,
 ) -> None:
-    draw()
-    for w in pyglet.app.windows:
-        w.switch_to()
-        w.dispatch_events()
-        w.dispatch_event("on_draw")
-        w.flip()
-
-
-def _import_pyglet(
-    _obj: unreal.UObject,
-    _args: unreal.WrappedStruct,
-    _ret: Any,
-    _func: unreal.BoundFunction,
-) -> None:
-    global pyglet, gl, pyglet_backend  # noqa: PLW0603
-    import pyglet  # type: ignore
-    from imgui_bundle.python_backends import pyglet_backend
-    from pyglet import gl  # type: ignore
-
-    remove_hook("WillowGame.FrontendGFxMovie:Start", Type.POST_UNCONDITIONAL, "blimgui_import_once")
-    add_hook("WillowGame.WillowGameViewportClient:Tick", Type.POST_UNCONDITIONAL, "blimgui_tick", callback=_on_tick)
-
-
-add_hook("WillowGame.FrontendGFxMovie:Start", Type.POST_UNCONDITIONAL, "blimgui_import_once", callback=_import_pyglet)
+    if hello_imgui.is_using_hello_imgui():
+        immapp.manual_render.render()
+        if should_close:
+            hello_imgui.get_runner_params().app_shall_exit = True
+            immapp.manual_render.tear_down()
 
 
 @keybind("Test Window", "F1")
 def test_window() -> None:
-    if WINDOW:
+    if hello_imgui.is_using_hello_imgui():
         close_window()
         return
     create_window("Test Window")
@@ -184,6 +161,14 @@ def test_window() -> None:
 
 mod = build_mod(
     cls=Library,
+    options=[
+        size_x,
+        size_y,
+        imgui_theme,
+    ],
+    hooks=[
+        _on_tick,
+    ],
     keybinds=[
         test_window,
     ],
